da1e8a8a35e6f745fb655c25d9f55a685ffaaa8f
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-06 22:44:28 +0300

    Решение 4й задачи

---
+++ Example004/Program.cs
@@ -0,0 +1,26 @@
+﻿// программа на вход принимает натурально число N,
+// на выходе показывает его цифры через запятые
+
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 10)
+{
+    Console.WriteLine(N);
+}
+else
+{
+    while (N > 0)
+    {
+        int first = N % 10;
+        N /= 10;
+        if (N > 0)
+        {
+            Console.Write(first + ",");
+        }
+        else
+        {
+            Console.Write(first);
+        }
+    }
+}
