86b24fecc71a9710f49606dde13ab09ba9790dfc
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-06 21:24:25 +0300

    Решение 3й задачи

---
+++ Example003/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа принимает на вход целое число из отрезка [10, 99]
+// показывает наибольшую цифру числа
+
+Console.Write("Введите целое число из отрезка [10, 98]: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int a1 = a / 10;
+int a2 = a % 10;
+
+if(a1 > a2)
+{
+    Console.WriteLine(a1);
+}
+else if(a1 < a2 )
+{
+    Console.WriteLine(a2);
+}
+else 
+{
+    Console.WriteLine("Числа равны");
+}
+
+if (a< 10 || a>98)
+{
+    Console.WriteLine($"Число {a} выходит за пределы допустимого отрезка");
+} 
