e6d4c166a4dccdd1a1a7ad8adaf0c8ff9daf1800
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-06 17:02:31 +0300

    Решение первой задачи практической работы ко второму семинару по курсу введение в ЯП

---
+++ Example001/Program.cs
@@ -0,0 +1,13 @@
+﻿// принимает на вход число и проверяет, кратно ли оно 7 && 23
+
+int num = 161;
+int cond1 = 7;
+int cond2 = 23;
+
+if (num % cond1 == 0 && num % cond2 == 0)
+{
+    Console.WriteLine($"Число {num} - кратно {cond1} и {cond2}");
+}
+else{
+    Console.WriteLine($"Число {num} не кратно {cond1} и {cond2}");
+}
