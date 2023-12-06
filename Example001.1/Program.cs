f3807f2c74fbb530cb97ad951694638450177235
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-06 17:17:13 +0300

    Второй способ решения первой задачи, чере ввод значения данных пользователем в терминале

---
+++ Example001.1/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2ой способ через ввод значения пользователем: 
+// принимает на вход число 
+// и проверяет, кратно ли оно 7 && 23
+
+Console.Write("Введите число num: ");
+int num = int.Parse(Console.ReadLine());
+int cond1 = 7;
+int cond2 = 23;
+
+if (num % cond1 == 0 && num % cond2 == 0)
+{
+    Console.WriteLine($"Число {num} - кратно {cond1} и {cond2}");
+}
+else{
+    Console.WriteLine($"Число {num} не кратно {cond1} и {cond2}");
+}
